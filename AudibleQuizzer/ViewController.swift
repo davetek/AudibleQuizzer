d760c606a1bfd36592c08cb0895bc842a8c9093d
davetek <dlawrence4994@gmail.com>
2020-07-25 19:56:14 -0400

    added tag enum, added tags to QandA instances, and revised functionality to ask questions only once until all questions have been asked, then repeat cycle

--- AudibleQuizzer/ViewController.swift
+++ AudibleQuizzer/ViewController.swift
@@ -14,6 +14,8 @@
 import UIKit
 import AVFoundation
 
+
+
 class ViewController: UIViewController {
 
     struct Question {
@@ -23,19 +25,30 @@ class ViewController: UIViewController {
         var source: String?
     }
 
-    struct QandA {
+    struct QandA: Equatable, Hashable {
         var question: String
         var answer: String
-        var tags: [String]?
+        var tags: [Tag]?
         var source: String?
     }
+    
+    enum Tag {
+        case intvCitibank202004
+        case iOS
+        case ObjC
+        case Swift
+        case UIKit
+    }
 
-    let qa01 = QandA(question: "What is app lifecycle for an iOS application?", answer: "An iOS app progresses through different states from launch to termination, and responds to system notifications differently depending on its state. These states are: inactive or background to active to inactive, then suspended to not running", tags: [], source: "Apple doc")
-    let qa02 = QandA(question: "How do you pass information from one view controller to another?", answer: "One method of passing information from one view controller to another is with dependency injection, which can be done at initialization or with a property assignment. The parent view controller assigns values in the child view controller; this enforces loose coupling between parent and child", tags: nil, source: nil)
-    let qa03 = QandA(question: "How do you define the run loop?", answer: "The run loop is a mechanism for managing and responding to events in an event-driven application. It is a process that continuously monitors for input events, and assigns each event that occurs to the appropriate target for processing.", tags: nil, source: nil)
-    let qa04 = QandA(question: "Can you describe coding keys?", answer: "Coding keys are nested enumerations that can be declared in a Codeable type, to specify the properties that must be included when instances of the type are encoded or decoded.", tags: nil, source: nil)
+
+    let qa01 = QandA(question: "What is app lifecycle for an iOS application?", answer: "An iOS app progresses through different states from launch to termination, and responds to system notifications differently depending on its state. These states are: inactive or background to active to inactive, then suspended to not running", tags: [.intvCitibank202004, .iOS], source: "Apple doc")
+    let qa02 = QandA(question: "How do you pass information from one view controller to another?", answer: "One method of passing information from one view controller to another is with dependency injection, which can be done at initialization or with a property assignment. The parent view controller assigns values in the child view controller; this enforces loose coupling between parent and child", tags: [.intvCitibank202004, .iOS], source: nil)
+    let qa03 = QandA(question: "How do you define the run loop?", answer: "The run loop is a mechanism for managing and responding to events in an event-driven application. It is a process that continuously monitors for input events, and assigns each event that occurs to the appropriate target for processing.", tags: [.intvCitibank202004, .iOS], source: nil)
+    let qa04 = QandA(question: "Can you describe coding keys?", answer: "Coding keys are nested enumerations that can be declared in a Codeable type, to specify the properties that must be included when instances of the type are encoded or decoded.", tags: [.intvCitibank202004, .Swift], source: nil)
+    let qa05 = QandA(question: "can you describe 'app pinning'?", answer: "App pinning, also known as SSL pinning, is a technique for making mobile apps more secure, even when users use unsecure wireless networks. The target server's SSL certificate is saved in the app bundle. This helps prevent man-in-the-middle attacks by ensuring that the app only communicates with the intended server.", tags: [.intvCitibank202004], source: nil)
     
     var questionsAndAnswers = [QandA]()
+    var currentQuestionsAndAnswers = Set<QandA>()
     var currentQandA: QandA?
     
     @IBOutlet weak var questionButton: UIButton!
@@ -43,13 +56,13 @@ class ViewController: UIViewController {
         guard let qAndA = pickRandomQandA(from: questionsAndAnswers) else {
             return
         }
+
         currentQandA = qAndA
         readQuestion(from: qAndA)
     }
     
     
     @IBOutlet weak var answerButton: UIButton!
-    
     @IBAction func sayAnswer(_ sender: UIButton) {
         guard let qAndA = currentQandA else {
             return
@@ -57,20 +70,31 @@ class ViewController: UIViewController {
         readAnswer(from: qAndA)
     }
     
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        questionsAndAnswers = [qa01, qa02, qa03, qa04]
-
+        questionsAndAnswers = [qa01, qa02, qa03, qa04, qa05]
     }
 
     func pickRandomQandA(from collection: [QandA]) -> QandA? {
+        //monitor the set of questions already asked; if all have been
+        // asked, clear the set to start over
+        if currentQuestionsAndAnswers.count == collection.count {
+            currentQuestionsAndAnswers.removeAll()
+        }
         guard let randomQandA = collection.randomElement() else {
             return nil
         }
-        return randomQandA
+        //if the question was already asked in this cycle, skip it
+        // if not, add the question & answer to the set of q&a already used
+        if currentQuestionsAndAnswers.contains(randomQandA) {
+            return pickRandomQandA(from: collection)
+        } else {
+            currentQuestionsAndAnswers.insert(randomQandA)
+            return randomQandA
+        }
+        
     }
 
     func readQuestion(from qAndA: QandA) {
